b855e4574619d1fc8b3a4d52c1a47a38d79bfe35
vityacasf <neustupov01@mail.ru>
2023-03-25 08:52:25 +0300

    add services

--- api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -1,11 +1,11 @@
-package shikimori.springboot;
+package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApiGatewayApplication {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(ApiGatewayApplication.class);
   }
 }
